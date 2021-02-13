0a647d1dfda4bbf12b78fdacc8b5918dfaa47686
Pedro H Santos B <pedrosaints@alu.ufc.br>
2021-02-12 22:40:17 -0300

    add funcao geradora de pop e comp de colisoes

---
+++ Trabalho2/2021Trabalho2-415083-AlgGenetico.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+n = 4
+
+
+
+def verificaConflito(A):
+	cont = 0
+	for i in range(n):
+		for j in range(n):
+			if A[i][j] == 1:
+				#verifica linha
+				for k in range(n):
+					if k != j:
+						if A[i][k] == 1:
+							cont+=1
+				#verifica coluna
+				for k in range(n):
+					if k != i:
+						if A[k][j] == 1:
+							cont+=1
+				#verifica diagonais
+				for k in range(1,n-i):
+					if j+k < n and i+k < n:
+						if A[i+k][j+k] == 1:
+							cont+=1				
+				for k in range(1,i+1):
+					if j-k > -1 and i-k > -1:
+						if A[i-k][j-k] == 1:
+							cont+=1	
+				for k in range(1,i+1):
+					if i-k > -1and j+k < n:
+						if A[i-k][j+k] == 1:
+							cont+=1
+				for k in range(1,n-i):
+					if j-k > -1and i+k < n:
+						if A[i+k][j-k] == 1:
+							cont+=1
+	return cont
+
+def produzElemento():
+	A = np.array((1, 1, 1, 1
+				  , 0, 0, 0, 0
+				  , 0, 0, 0, 0
+				  , 0, 0, 0, 0))
+	np.random.shuffle(A)
+	A = A.reshape((n, n))
+	return A
+
+def criaPopulacao(tamanho):
+	p = []
+	for i in range(tamanho):
+		p.append(produzElemento())
+	return p
+
+pop = criaPopulacao(10)
+
+for elem in pop:
+	print()
+	print("ENTRADA:")
+	print(elem)
+	print()
+	print("NUMERO DE COLISOES:")
+	print(verificaConflito(elem))
